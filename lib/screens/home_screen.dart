b1c2d0ff492702f050107dd8a2e05e664e613e3b
zy-vy <jifidoank@gmail.com>
2022-11-09 12:20:28 +0700

    Home Page

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -156,7 +156,63 @@ class _HomeScreenState extends State<HomeScreen> {
               )
             ],
           ),
-          // GridView(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount)
+          GridView.builder(
+              shrinkWrap: true,
+              itemCount: 2,
+              itemBuilder: (ctx, i) {
+                return Card(
+                  child: Container(
+                    // height: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50)
+                    ),
+                    margin: EdgeInsets.all(10),
+                    padding: EdgeInsets.all(10),
+                    child: Stack(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+
+                          children: [
+                            Expanded(
+                              child: Image.network(
+                                // height: 100,
+                                "https://picsum.photos/id/30/200",
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.symmetric(vertical: 5),
+                              child: Text('Raket',style: TextStyle( fontSize: 18),),
+                            )  ,
+
+                            const Padding(
+                              padding: EdgeInsets.symmetric(vertical: 5),
+                              child: Text("Rp. 150.000/hour",style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 5),
+                              child: Row(
+                                children: const [
+                                  Text("Detail",)
+                                ],
+                              ),
+                            )
+
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 1.0,
+                crossAxisSpacing: 0.0,
+                mainAxisSpacing: 5,
+                mainAxisExtent: 264,
+              ))
         ],
       ),
     ));
